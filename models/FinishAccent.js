ee61a77b59df7f8dd08666579721b85c71a17dc3
github username <github email address>
2020-04-16 08:22:50 -0600

    added seeds for flowers

---
+++ models/FinishAccent.js
@@ -0,0 +1,20 @@
+const FinishStyle = require('./FinishStyle')
+const { Model } = require('objection')
+
+
+class FinishAccent extends Model {
+    static tableName = 'finish_accents';
+  
+    static relationMappings = {
+      finish_accent: {
+        relation: Model.HasManyRelation,
+        modelClass: FinishStyle,
+        join: {
+          from: 'finish_accents.id',
+          to: 'finish_styles.finish_accent_id'
+        }
+      }
+    };
+  }
+
+  module.exports = FinishAccent;
