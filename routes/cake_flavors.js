5d6eaa11131b0e2849f7a44285a13744827da2bd
github username <github email address>
2020-04-21 14:04:38 -0600

    git ignore env

--- routes/cake_flavors.js
+++ routes/cake_flavors.js
@@ -8,6 +8,7 @@ const CakeFlavor = require("../models/CakeFlavor")
 router.get('/', async (request, response) => {
     const cake_flavors = await CakeFlavor.query()
     response.json({ cake_flavors })
+    console.log("hello")
 })
 
 
