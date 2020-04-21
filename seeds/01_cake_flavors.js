fe8a474d257f2b715483b9082b1968f01666f09c
github username <github email address>
2020-04-21 13:50:37 -0600

    update nodemon

--- seeds/01_cake_flavors.js
+++ seeds/01_cake_flavors.js
@@ -4,17 +4,17 @@ exports.seed = function(knex) {
   return knex('cake_flavors').del()
     .then(function () {
       return knex('cake_flavors').insert([
-        {name: 'Chocolate', image: 'https://i.ibb.co/bX9RJGB/chocolate.png', icon:'https://i.ibb.co/wzN4MQx/1.png', cake_color: '#46322b' },
-        {name: 'Red Velvet', image: 'https://i.ibb.co/513fKNW/redvelvet.png', icon:'https://i.ibb.co/Qrzhktf/2.png', cake_color: '#9d0b0b'  },
-        {name: 'Vanilla', image: 'https://i.ibb.co/D7pFWY9/vanilla.png', icon:'https://i.ibb.co/MBg8htL/7.png', cake_color: '#fcf5ee'  },
-        {name: 'Lemon', image: 'https://i.ibb.co/0Xr6gX5/lemon.png', icon:'https://i.ibb.co/y6YHYq7/3.png', cake_color: '#fff591'  },
-        {name: 'Raspberry', image: 'https://i.ibb.co/jDCvJKz/raspberry.png', icon:'https://i.ibb.co/sv4S2sL/5.png', cake_color: '#eca0b6'  },
-        {name: 'Carrot', image: 'https://i.ibb.co/qBrrjC7/carrot.png', icon:'https://i.ibb.co/dfWNDQM/10.png', cake_color: '#f18867'  },
-        {name: 'Funfetti', image: 'https://i.ibb.co/HXfgBTW/funfetti.png', icon:'https://i.ibb.co/HGVGz57/6.png', cake_color: '#fcf5ee'  },
-        {name: 'Pumpkin', image: 'https://i.ibb.co/LSWt4d9/pumpkin.png', icon:'https://i.ibb.co/jGzjG1J/8.png', cake_color: '#ecab69'  },
-        {name: 'Almond', image: 'https://i.ibb.co/sWqZyJm/almond.png', icon:'https://i.ibb.co/DfVVxFS/12.png', cake_color: '#fff1c1'  },
-        {name: 'Spice', image: 'https://i.ibb.co/XZk20Cs/spice.png', icon:'https://i.ibb.co/DgsmjXx/9.png', cake_color: '#e3c4a8'  },
-        {name: 'Angel Food', image: 'https://i.ibb.co/JRnfRLD/angel.png', icon:'https://i.ibb.co/27zj45W/11.png', cake_color: '#fcf9f9'  }
+        {name: 'Chocolate', image: 'https://i.imgur.com/8kYaj9c.png', icon:'https://i.imgur.com/86f0DxW.png', cake_color: '#46322b' },
+        {name: 'Red Velvet', image: 'https://i.imgur.com/FHwT4MV.png', icon:'https://i.imgur.com/7U6gaMe.png', cake_color: '#9d0b0b'  },
+        {name: 'Vanilla', image: 'https://i.imgur.com/qPRht4P.png', icon:'https://i.imgur.com/gI4ZbN9.png', cake_color: '#fcf5ee'  },
+        {name: 'Lemon', image: 'https://i.imgur.com/kPWos1a.png', icon:'https://i.imgur.com/PyNpUTS.png', cake_color: '#fff591'  },
+        {name: 'Raspberry', image: 'https://i.imgur.com/HjxqR7m.png', icon:'https://i.imgur.com/UltIO6P.png', cake_color: '#eca0b6'  },
+        {name: 'Carrot', image: 'https://i.imgur.com/VIODiFZ.png', icon:'https://i.imgur.com/pPJypBe.png', cake_color: '#f18867'  },
+        {name: 'Funfetti', image: 'https://i.imgur.com/V2SZ6XT.png', icon:'https://i.imgur.com/OoP8jgZ.png', cake_color: '#fcf5ee'  },
+        {name: 'Pumpkin', image: 'https://i.imgur.com/US6p2UM.png', icon:'https://i.imgur.com/I5op57n.png', cake_color: '#ecab69'  },
+        {name: 'Almond', image: 'https://i.imgur.com/AqlkTk8.png', icon:'https://i.imgur.com/mMW2MwQ.png', cake_color: '#fff1c1'  },
+        {name: 'Spice', image: 'https://i.imgur.com/zIIuLKv.png', icon:'https://i.imgur.com/BfyNB3N.png', cake_color: '#e3c4a8'  },
+        {name: 'Angel Food', image: 'https://i.imgur.com/CIpESCD.png', icon:'https://i.imgur.com/N3OuyVX.png', cake_color: '#fcf9f9'  }
       ]);
     });
 };
