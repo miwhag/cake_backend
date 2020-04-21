88c99a18a3c1c54f7b2a71da5a033e42d05afdde
github username <github email address>
2020-04-21 17:50:41 -0600

    drop accent styles migration

--- migrations/20200406141900_create_users_table.js
+++ migrations/20200406141900_create_users_table.js
@@ -1,17 +1,17 @@
 
 exports.up = function(knex) {
-    // return knex.schema.createTable("users", table => {
-        // table.increments();
-        // table.string("first_name");
-        // table.string("last_name");
-        // table.string("username");
-        // table.string("password_digest");
-    // })
+    return knex.schema.createTable("users", table => {
+        table.increments();
+        table.string("first_name");
+        table.string("last_name");
+        table.string("username");
+        table.string("password_digest");
+    })
   
 };
 
 exports.down = function(knex) {
-    // return knex.shema.dropTable("users")
+    return knex.shema.dropTable("users")
 };
 
 
